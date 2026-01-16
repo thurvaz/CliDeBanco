dc1ec035313d32ec4e3c70fedfc779a4dad1908b
thurvaz <thuivvaz@icloud.com>
2026-01-16 17:53:10 -0300

    feat: Created CreateAccount function and added it to the main func

---
+++ cmd/cli/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/thurvaz/CliDeBanco/internal"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:  "meuapp",
+		Args: cobra.MaximumNArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Banco Do Thurvaz")
+		},
+	}
+	rootCmd.AddCommand(internal.CreateAccount)
+	rootCmd.Execute()
+}
---
+++ internal/CreateAccount.go
@@ -0,0 +1,24 @@
+package internal
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var CreateAccount = &cobra.Command{
+	Use:  "createaccount [name] [cpf] [password]",
+	Args: cobra.MinimumNArgs(3),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var name string
+		var cpf string
+		var password string
+		if len(args) == 3 {
+			name = args[0]
+			cpf = args[1]
+			password = args[2]
+		}
+		fmt.Printf("Account Created, %s %s %s", name, cpf, password)
+		return nil
+	},
+}
